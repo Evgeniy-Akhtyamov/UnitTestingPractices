35fe22c0f3654e853d0debf5379c91c48804ce72
Evgeniy-Akhtyamov <Ahtyamove@mail.ru>
2021-09-13 20:49:12 +0400

    Добавьте файлы проекта.

---
+++ UnitTestingPractices.Tests/CalculatorTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingPractices.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            var calculatorTest = new Calculator();
+            Assert.That(calculatorTest.Additional(100, 20) == 120);
+        }
+
+        [Test]
+        public void SubtractionMustReturnCorrectValue()
+        {
+            var calculatorTest = new Calculator();
+            Assert.That(calculatorTest.Subtraction(100, 20) == 80);
+        }
+
+        [Test]
+        public void MiltiplicationMustReturnCorrectValue()
+        {
+            var calculatorTest = new Calculator();
+            Assert.That(calculatorTest.Miltiplication(100, 20) == 2000);
+        }
+
+        [Test]
+        public void DivisionMustReturnCorrectValue()
+        {
+            var calculatorTest = new Calculator();
+            Assert.That(calculatorTest.Division(100, 20) == 5);
+        }
+
+        [Test]
+        public void DivisionMustThrowException()
+        {
+            var calculatorTest = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calculatorTest.Division(100, 0));
+        }
+    }
+}
